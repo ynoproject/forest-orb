1b76e58d7cd96ec2a53cc0cfb108be9c0469b901
kekami <kekami@kekami.dev>
2021-11-21 19:56:45 -0500

    Initial commit

---
+++ chat.js
@@ -0,0 +1,50 @@
+Module['onRuntimeInitialized'] = ChatInit
+ENV.SDL_EMSCRIPTEN_KEYBOARD_ELEMENT = "#canvas"
+
+function chatboxAddMessage(msg) {
+  messages = document.getElementById("messages")
+  
+  scroll = (messages.scrollHeight - messages.scrollTop === messages.clientHeight)
+
+  msgContainer = document.createElement("div")
+  msgContainer.classList.add("message")
+  msgContainer.appendChild(document.createTextNode(msg))
+
+  messages.appendChild(msgContainer)
+
+  if (scroll) {
+    messages.scrollTop = messages.scrollHeight
+  }
+}
+
+function chatInputActionFired() {
+  if (chatInput.value === "") {
+    return
+  }
+  chatInput = document.getElementById("chatInput")
+  ptr = Module.allocate(Module.intArrayFromString(chatInput.value), Module.ALLOC_NORMAL)
+  Module._SendChatMessageToServer(ptr)
+  Module._free(ptr)
+  chatInput.value = ""
+}
+
+function chatNameCheck() {
+  if (nameInput.value == "") return;
+  if (!(/^[A-Za-z0-9]+$/.test(nameInput.value))) return;
+  document.getElementById("enterNameContainer").style.display = 'none';
+  document.getElementById("chatInput").disabled = false
+  nameInput = document.getElementById("nameInput")
+  ptr = Module.allocate(Module.intArrayFromString(nameInput.value), Module.ALLOC_NORMAL)
+  Module._ChangeName(ptr)
+  Module._free(ptr)
+  nameInput.value = ""
+}
+
+function ChatInit() {
+  document.getElementById("chatbox").style.display = 'block'
+}
+
+//called from easyrpg player
+function GotChatMsg(msg) {
+  chatboxAddMessage(msg)
+}
