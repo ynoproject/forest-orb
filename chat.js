e2a0a13c78042c17b3b2a95cbea280bdab019872
Flashfyre <flashfireex@gmail.com>
2022-01-15 11:31:44 -0500

    Add markdown support for chat

--- chat.js
+++ chat.js
@@ -12,7 +12,8 @@ function chatboxAddMessage(msg) {
   const message = document.createElement("span");
   message.classList.add("message");
   
-  message.appendChild(document.createTextNode(msg));
+  populateMessageNodes(parseMessageTextForMarkdown(msg), message);
+
   msgContainer.appendChild(message);
   messages.appendChild(msgContainer);
 
@@ -47,6 +48,44 @@ function chatNameCheck() {
 
 function initChat() {
   document.getElementById("chatboxContainer").style.display = "table-cell";
+}
+
+function parseMessageTextForMarkdown(msg) {
+  const replacements = [
+    { p: /<\/?[bisu] *>/ig, r: '' },
+    { p: /\*\*(.*?)\*\*/ig, r: '<b>$1</b>' },
+    { p: /\*(.*?)\*/ig, r: '<i>$1</i>' },
+    { p: /\_\_(.*?)\_\_/ig, r: '<u>$1</u>' },
+    { p: /\_(.*?)\_/ig, r: '<i>$1</i>' },
+    { p: /\~\~(.*?)\~\~/ig, r: '<s>$1</s>' },
+  ];
+  for (let e of replacements)
+    msg = msg.replace(e.p, e.r);
+
+  return msg;
+}
+
+function populateMessageNodes(msg, node) {
+  const tagPattern = /<([bisu])>(.*?)<\/\1>/;
+  let cursor = 0;
+  let result;
+  
+  while ((result = tagPattern.exec(msg.slice(cursor)))) {
+    if (result.index) {
+      const textNode = document.createTextNode(msg.slice(cursor, cursor + result.index));
+      node.appendChild(textNode);
+    }
+    const childNode = document.createElement(result[1]);
+    const innerMsg = msg.substr(cursor + result.index + 3, result[2].length);
+    populateMessageNodes(innerMsg, childNode);
+    node.appendChild(childNode);
+    cursor += result.index + result[2].length + 7;
+  }
+
+  if (cursor < msg.length) {
+    const textNode = document.createTextNode(msg.slice(cursor));
+    node.appendChild(textNode);
+  }
 }
 
 //called from easyrpg player
