29fc6be2472d16b62ec35ab08a04b0110c0d077f
Flashfyre <flashfireex@gmail.com>
2022-06-04 00:38:31 -0400

    Move synced pictures call

--- play.js
+++ play.js
@@ -1439,7 +1439,6 @@ loadOrInitCache();
 
 initDefaultSprites();
 updateBadges();
-fetchAndSendSyncedPictures();
 if (typeof initBadgeTools === 'function')
   initBadgeTools();
 fetchAndPopulateYnomojiConfig();
