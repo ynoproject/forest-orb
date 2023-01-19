16ab596dce1b311f5a1bd0822927d46cd5b0dd8a
Flashfyre <flashfireex@gmail.com>
2023-01-19 10:24:26 -0500

    Add missed constant rename in rankings

--- rankings.js
+++ rankings.js
@@ -23,7 +23,7 @@ function initRankingControls() {
 
 function rankingsApiFetch(path) {
   return new Promise((resolve, reject) => {
-    const sId = getCookie(SESSION_ID_KEY);
+    const sId = getCookie(sessionIdKey);
     const headers = sId ? { 'Authorization': sId } : {};
     fetch(`${rankingsUrl}/${path}`, { headers: headers })
       .then(response => resolve(response))
