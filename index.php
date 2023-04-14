5cc5faf41bb03a6acfeaa384b15ce5a2e69fca9d
Carbonara <108797333+Mimigris@users.noreply.github.com>
2023-04-15 00:47:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -419,10 +419,10 @@
                       <option value="pt">Português</option>
                       <option value="fr">Français</option>
                       <option value="de">Deutsch</option>
+                      <option value="it">Italiano</option>
                       <option value="ru">Русский</option>
                       <option value="vi">Tiếng Việt</option>
                       <option value="ar">العربية</option>
-                      <option value="it">Italiano</option>
                     </select>
                   </div>
                   <label id="noGameLocInstruction" class="hidden" data-i18n="[html]instruction.noGameLoc">* Game Localization Unsupported</label>
