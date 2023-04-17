fa440d39eda78d104425a0a7fa6677e35098aa7b
Carbonara <108797333+Mimigris@users.noreply.github.com>
2023-04-17 08:54:14 +0200

    Add Italian localisation to YNOproject by cry (#290)
    * Add files via upload
    
    * Update index.php
    
    * Update fr.json
    
    * Update index.php

--- index.php
+++ index.php
@@ -419,6 +419,7 @@
                       <option value="pt">Português</option>
                       <option value="fr">Français</option>
                       <option value="de">Deutsch</option>
+                      <option value="it">Italiano</option>
                       <option value="ru">Русский</option>
                       <option value="vi">Tiếng Việt</option>
                       <option value="ar">العربية</option>
