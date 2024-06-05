a21e9391bc210c92c696143348e5610b9023744e
krossower1 <121849382+krossower1@users.noreply.github.com>
2024-06-05 09:50:43 +0200

    Add info display next to time trial badge section

--- badgetools.js
+++ badgetools.js
@@ -221,7 +221,13 @@ function initBadgeTools() {
         if (this.gameId === '2kki') {
           return [
             { key: '2_el', label: 'Events and Locations' },
-            { key: '3_tt', label: 'Time Trials' },
+            { key: '3_tt', label: 'Time Trials <a href="javascript:void(0);" class="helpLink iconLink" data-i18n="[title]modal.settings.fields.timeTrialinfo.helpText">
+                    <div class="helpIcon icon fillIcon invertFillIcon altIcon">
+                      <svg viewBox="0 0 18 18">
+                        <path d="m9 0a1 1 90 0 0 0 18 1 1 90 0 0 0-18m-1.25 10.25a1 1 90 0 0 2.5 0.5q0.25-1 1.25-1.5c0.75-0.5 2.5-1.5 2.5-3.75 0-4-7.75-5.5-9.5-0.5a0.25 0.25 90 0 0 2.75 0.5c0.25-1.75 4-2.25 3.75 0.5 0 1.5-3 2.25-3.25 4.25m1.25 6a0.25 0.25 90 0 0 0-3.25 0.25 0.25 90 0 0 0 3.25" />
+                      </svg>
+                    </div>
+                  </a>' },
             { key: '4_ch', label: 'Challenges' },
             { key: '5_end', label: 'End Game' }
           ];
