ae1dee3607bdfe9dbe1e9d4348a1dec876c09fe2
krossower1 <121849382+krossower1@users.noreply.github.com>
2024-06-16 11:32:20 +0200

    Update account.js

--- account.js
+++ account.js
@@ -229,7 +229,7 @@ function updateModControls() {
   }
 }
 
-setInterval(showSaveSyncToastMessage('saveReminder', 'save', 1), globalConfig.saveReminder * 60000);
+setInterval(showToastMessage(getMassagedLabel(localizedMessages.toast.saveSync['saveReminder'], true), 'save', true), (globalConfig.saveReminder * 60000));
 
 function showAccountToastMessage(key, icon, username) {
   if (!notificationConfig.account.all || (notificationConfig.account.hasOwnProperty(key) && !notificationConfig.account[key]))
