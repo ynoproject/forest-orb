e63d76a7f825ab762ed2d43f2c7f5b7afaa2254e
Viet Dinh <54ckb0y789@gmail.com>
2024-08-16 23:58:03 -0400

    Fix save modal info popup

--- index.php
+++ index.php
@@ -1017,7 +1017,12 @@
         <div id="saveModal" class="modal wideModal hidden">
           <a href="javascript:void(0);" class="modalClose">✖</a>
           <div class="modalHeader">
-            <h1 class="modalTitle" data-i18n="[html]modal.save.title">Manage Save Data</h1> <a href='javascript:void(0);' class='helpLink iconLink' data-i18n='[title]save.info'> <div class='helpIcon icon fillIcon invertFillIcon altIcon'> <svg viewBox='0 0 18 18'> <path d='m9 0a1 1 90 0 0 0 18 1 1 90 0 0 0-18m-1.25 10.25a1 1 90 0 0 2.5 0.5q0.25-1 1.25-1.5c0.75-0.5 2.5-1.5 2.5-3.75 0-4-7.75-5.5-9.5-0.5a0.25 0.25 90 0 0 2.75 0.5c0.25-1.75 4-2.25 3.75 0.5 0 1.5-3 2.25-3.25 4.25m1.25 6a0.25 0.25 90 0 0 0-3.25 0.25 0.25 90 0 0 0 3.25' /> </svg> </div> </a>
+            <h1 class="modalTitle">
+              <span data-i18n="[html]modal.save.title">
+                Manage Save Data
+              </span>
+              <a href='javascript:void(0);' class='helpLink iconLink' data-i18n='[title]modal.save.info'> <div class='helpIcon icon fillIcon invertFillIcon altIcon'> <svg viewBox='0 0 18 18'> <path d='m9 0a1 1 90 0 0 0 18 1 1 90 0 0 0-18m-1.25 10.25a1 1 90 0 0 2.5 0.5q0.25-1 1.25-1.5c0.75-0.5 2.5-1.5 2.5-3.75 0-4-7.75-5.5-9.5-0.5a0.25 0.25 90 0 0 2.75 0.5c0.25-1.75 4-2.25 3.75 0.5 0 1.5-3 2.25-3.25 4.25m1.25 6a0.25 0.25 90 0 0 0-3.25 0.25 0.25 90 0 0 0 3.25' /> </svg> </div> </a>
+            </h1>
           </div>
           <div class="modalContent">
             <div id="saveSlotList" class="scrollableContainer">
