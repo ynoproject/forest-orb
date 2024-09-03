b003daf0752121a5f2b7d5399a96cd7dd8739950
Viet Dinh <36768030+Desdaemon@users.noreply.github.com>
2024-09-02 22:46:57 -0400

    Hotfix for #534 (#536)
    * Fix Cache reset buttons
    
    * Correct button i18n string
    
    * Adjust default save reminder interval

--- play.js
+++ play.js
@@ -71,7 +71,7 @@ let globalConfig = {
   questionablePreloads: false,
   rulesReviewed: false,
   badgeToolsData: null,
-  saveReminder: 15
+  saveReminder: 60,
 };
 
 let config = {
@@ -2286,7 +2286,7 @@ function setCacheValue(cacheType, key, value) {
 
 /** @param {HTMLElement?} el */
 function clearCache(cacheType, el) {
-  if (el) el.setAttribute('disabled');
+  if (el) el.setAttribute('disabled', '');
   cache[cacheType] = {};
   updateCache(cacheType);
   switch (cacheType) {
