bf1cae6045c167bf677a446e4a0e501686309116
Flashfyre <flashfireex@gmail.com>
2024-09-23 21:21:04 -0400

    Add depth and new filters to locations list

--- events.js
+++ events.js
@@ -82,6 +82,18 @@ function onUpdateEvents(events, ignoreLocationCheck) {
 
   for (let eventType of eventTypes) {
     eventsCache[eventType] = events[eventType].map(l => {
+      if (eventType === 'locations' && l.game === '2kki') {
+        let adjustedDepth = Math.floor(l.depth / 3) * 2;
+        if (l.depth % 3 === 2)
+          adjustedDepth++;
+        l.depth = Math.min(adjustedDepth, 10);
+        if (l.minDepth) {
+          let adjustedMinDepth = Math.floor(l.minDepth / 3) * 2;
+          if (l.minDepth % 3 === 2)
+            adjustedMinDepth++;
+          l.minDepth = Math.min(adjustedMinDepth, 10);
+        }
+      }
       l.endDate = new Date(l.endDate);
       return l;
     });
