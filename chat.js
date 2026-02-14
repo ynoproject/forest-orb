26a1c6926d003e25fec2a71a1a7b42f0bf8af8f2
flan <flan@flande.re>
2026-02-14 21:38:09 +0100

    Check chat message length on compositionend as well

--- chat.js
+++ chat.js
@@ -477,11 +477,14 @@ function chatInputActionFired() {
   document.getElementById("ynomojiContainer").classList.add("hidden");
 }
 
-/** @param {number} length */
-function constrainByteLength(length) {
+/**
+ * @param {HTMLInputElement} inputElement
+ * @param {number} length
+ */
+function constrainByteLength(inputElement, length) {
   const buf = new Uint8Array(length);
   const enc = new TextEncoder();
-  return event => {
+  const listener = event => {
     const target = event?.target;
     if (!target) return;
     // modifying input contents during composition would interrupt it
@@ -500,9 +503,14 @@ function constrainByteLength(length) {
     target.value = v.substring(0, e1.read) + v.substring(sel, sel+e2.read);
     target.selectionEnd = e1.read;
   };
+
+  // some browsers send an input event with isComposing: false after composition
+  // finishes but it's not guaranteed to always fire (and doesn't on e.g. chromium)
+  // so we have to listen for compositionend as well
+  ['input', 'compositionend'].forEach(e => inputElement.addEventListener(e, listener));
 }
 
-document.getElementById('chatInput').addEventListener('input', constrainByteLength(150));
+constrainByteLength(document.getElementById('chatInput'), 150);
 
 function chatNameCheck() {
   trySetChatName(document.getElementById("nameInput").value);
